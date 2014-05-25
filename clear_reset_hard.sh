ba1af4b10c0d66d21d23f83846c956a3a8298dbb
Michael Stutz <michaeljstutz@gmail.com>
2014-05-25 18:46:13 -0400

    Added a script for clearing and reseting the repo (makes testing easier :)

---
+++ clear_reset_hard.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "removing all files and directories except for .git and this script.."
+find . -maxdepth 1 -not -name ".git" -not -name "clear_reset_hard.sh" -not -name "." -exec rm -rf {} \;
+git reset --hard
