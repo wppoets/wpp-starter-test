06ced57d2d40c8909707a0a5fab7892feddac20c
Michael Stutz <michaeljstutz@gmail.com>
2014-05-25 20:37:02 -0400

    Added git modules directory to the clearing script

--- clear_reset_hard.sh
+++ clear_reset_hard.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 echo "removing all files and directories except for .git and this script.."
 find . -maxdepth 1 -not -name ".git" -not -name "clear_reset_hard.sh" -not -name "." -exec rm -rf {} \;
+find ./.git/modules/* -maxdepth 1 -exec rm -rf {} \;
 git reset --hard
